9e8a63ce949fa5f6664fe10bc4f0fdadff14369e
Ananiev_IM <ilya.anunev@gmail.com>
2024-10-07 20:41:59 +0700

    'fixing style'

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,6 @@
 public class BonusMilesService {
-    public int calculate(int ticketPrice)
-    {
-        int miles = ticketPrice/20;
+    public int calculate(int ticketPrice) {
+        int miles = ticketPrice / 20;
         return miles;
     }
 }
